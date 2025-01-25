52f1cfffd7b0fe02c59ce1df1f5fad177df6c09a
kavina-a <kmanthuka@gmail.com>
2025-01-25 10:58:33 +0530

    vercel updates

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,10 +1,16 @@
-export const BASE_URL = '';  //localhost:5000 //basically the backend url
-export const USERS_URL = '/api/users';
-export const CATEGORY_URL = '/api/category';
-export const PRODUCTS_URL = '/api/product';
-export const UPLOAD_URL = '/api/uploads';
-export const ORDERS_URL = '/api/order';
-export const STRIPE_URL = '/api/stripe';
-export const SELLER_URL = '/api/seller';
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-export const STRIPE_PUBLISHABLE_KEY = 'pk_test_51Q5MsnKcg0aY6eUJ57igd8egjQBvGBfi6A8dhkvhXz0hGL9JgrkTQegIbwmuTcFCIvKCQeEsB5RtQrHe4SMjntOI0092wwr405';
+export const BASE_URL = isDevelopment
+  ? 'http://localhost:5001'
+  : 'https://earthify-backend.vercel.app'; 
+
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const PRODUCTS_URL = `${BASE_URL}/api/product`;
+export const UPLOAD_URL = `${BASE_URL}/api/uploads`;
+export const ORDERS_URL = `${BASE_URL}/api/order`;
+export const STRIPE_URL = `${BASE_URL}/api/stripe`;
+export const SELLER_URL = `${BASE_URL}/api/seller`;
+
+export const STRIPE_PUBLISHABLE_KEY =
+  'pk_test_51Q5MsnKcg0aY6eUJ57igd8egjQBvGBfi6A8dhkvhXz0hGL9JgrkTQegIbwmuTcFCIvKCQeEsB5RtQrHe4SMjntOI0092wwr405';
